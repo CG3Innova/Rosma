3bdbcfc28f903c2170ba560a61f654f047905892
Javi Teja <j.teja@cg3innova.es>
2024-11-27 14:23:36 +0100

    fotos que faltaban y carrusel de servicios

--- includes/servicios.php
+++ includes/servicios.php
@@ -15,7 +15,7 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../con-buena-cara/index.php"><img src="../../images/resource/service-1.jpg" alt=""></a></div>
+                    <div class="image"><a href="../con-buena-cara/index.php"><img src="../../images/servicios/con-buena-cara.jpg" alt="Con buena cara - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-stones"></span></div>
@@ -29,7 +29,7 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../dame-tu-mano/index.php"><img src="../../images/resource/service-2.jpg" alt=""></a></div>
+                    <div class="image"><a href="../dame-tu-mano/index.php"><img src="../../images/servicios/dame-tu-mano.jpg" alt="dame tu mano - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-facial"></span></div>
@@ -43,7 +43,7 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../pisa-fuerte/index.php"><img src="../../images/resource/service-3.jpg" alt=""></a></div>
+                    <div class="image"><a href="../pisa-fuerte/index.php"><img src="../../images/servicios/pisa-fuerte.jpg" alt="Pisa fuerte - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-nail"></span></div>
@@ -57,7 +57,7 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../piel-de-seda/index.php"><img src="../../images/resource/service-1.jpg" alt=""></a></div>
+                    <div class="image"><a href="../piel-de-seda/index.php"><img src="../../images/servicios/piel-de-seda.jpg" alt="Piel de seda - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-stones"></span></div>
@@ -71,7 +71,7 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../fotodepilacion/index.php"><img src="../../images/resource/service-2.jpg" alt=""></a></div>
+                    <div class="image"><a href="../fotodepilacion/index.php"><img src="../../images/servicios/fotodepilacion.jpg" alt="Fotodepilacion - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-facial"></span></div>
@@ -85,7 +85,7 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../para-que-estes-mas-guapa/index.php"><img src="../../images/resource/service-3.jpg" alt=""></a></div>
+                    <div class="image"><a href="../para-que-estes-mas-guapa/index.php"><img src="../../images/servicios/para-que-estes-mas-guapa.jpg" alt="Para que estes más guapa - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-nail"></span></div>
@@ -99,7 +99,7 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../formando-curvas/index.php"><img src="../../images/resource/service-1.jpg" alt=""></a></div>
+                    <div class="image"><a href="../formando-curvas/index.php"><img src="../../images/servicios/formando-curvas.jpg" alt="Formando curvas - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-stones"></span></div>
@@ -113,7 +113,7 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../a-toda-velocidad/index.php"><img src="../../images/resource/service-2.jpg" alt=""></a></div>
+                    <div class="image"><a href="../a-toda-velocidad/index.php"><img src="../../images/servicios/a-toda-velocidad.jpg" alt="A toda velocidad - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-facial"></span></div>
@@ -127,7 +127,7 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../para-tu-mirada/index.php"><img src="../../images/resource/service-3.jpg" alt=""></a></div>
+                    <div class="image"><a href="../para-tu-mirada/index.php"><img src="../../images/servicios/para-tu-mirada.jpg" alt="Para tu mirada - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-nail"></span></div>
@@ -141,11 +141,11 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../da-el-do-de-pecho/index.php"><img src="../../images/resource/service-3.jpg" alt=""></a></div>
+                    <div class="image"><a href="../escote-provocador/index.php"><img src="../../images/servicios/escote-provocador.jpg" alt="Escote provocador - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-nail"></span></div>
-                    <h3><a href="../da-el-do-de-pecho/index.php">Da el DO de pecho</a></h3>
+                    <h3><a href="../escote-provocador/index.php">Escote provocador</a></h3>
                     <p>Stone Massage are the best most popular spa treatment.</p>
                 </div>
             </div>
@@ -155,21 +155,7 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../escote-de-infarto/index.php"><img src="../../images/resource/service-3.jpg" alt=""></a></div>
-                </div>
-                <div class="lower-content">
-                    <div class="icon-box"><span class="flaticon-nail"></span></div>
-                    <h3><a href="../escote-de-infarto/index.php">Escote de infarto</a></h3>
-                    <p>Stone Massage are the best most popular spa treatment.</p>
-                </div>
-            </div>
-        </div>
-
-        <!-- Services Block -->
-        <div class="service-block">
-            <div class="inner-box">
-                <div class="image-box">
-                    <div class="image"><a href="../para-que-te-mimes/index.php"><img src="../../images/resource/service-3.jpg" alt=""></a></div>
+                    <div class="image"><a href="../para-que-te-mimes/index.php"><img src="../../images/servicios/para-que-te-mimes.jpg" alt="Para que te mimes - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-nail"></span></div>
@@ -183,7 +169,7 @@
         <div class="service-block">
             <div class="inner-box">
                 <div class="image-box">
-                    <div class="image"><a href="../para-tu-bienestar/index.php"><img src="../../images/resource/service-3.jpg" alt=""></a></div>
+                    <div class="image"><a href="../para-tu-bienestar/index.php"><img src="../../images/servicios/para-tu-bienestar.jpg" alt="Para tu bienestar - Rosma Belleza"></a></div>
                 </div>
                 <div class="lower-content">
                     <div class="icon-box"><span class="flaticon-nail"></span></div>
