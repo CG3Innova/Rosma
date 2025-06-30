f442a043346a4c6880d719ddb3c82fc1a5416934
Javi Teja <j.teja@cg3innova.es>
2025-06-30 17:31:12 +0200

    foto cambiada

--- servicios/servicios-medicos/vitaminas.php
+++ servicios/servicios-medicos/vitaminas.php
@@ -59,7 +59,7 @@
                 <div class="content-side pull-right col-md-9 col-sm-12 col-xs-12">
                     <div class="service-detail">
                         <div class="image-box">
-                            <figure><img src="../../images/servicios/nutricion.jpg" alt="Nutrición - Rosma Belleza"></figure>
+                            <figure><img src="../../images/servicios/vitaminas.jpg" alt="vitaminas - Rosma Belleza"></figure>
                         </div>
                         <div class="lower-content">
                             <h2>Vitaminas</h2>
