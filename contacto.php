a33d12a7244e59a9d931be07c8320ad4985140d8
Javi Teja <j.teja@cg3innova.es>
2025-07-03 13:25:34 +0200

    favicones

--- contacto.php
+++ contacto.php
@@ -14,8 +14,12 @@
     <link id="theme-color-file" href="css/color-themes/default-theme.css" rel="stylesheet">
 
     <!--Favicon-->
-    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Rosma" />
+    <link rel="manifest" href="/images/favicon/site.webmanifest" />
 
     <!-- Responsive -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
