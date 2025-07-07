c852fa7ab94505d8ede7dfc890f5f6164b177788
Javi Teja <j.teja@cg3innova.es>
2025-07-07 18:06:55 +0200

    colocacion etiquetas seo bien

--- servicios/pisa-fuerte/pedicura-spa.php
+++ servicios/pisa-fuerte/pedicura-spa.php
@@ -2,8 +2,7 @@
 <html lang="es-ES">
 <head>
 <meta charset="utf-8">
-<title>Rosma Belleza | Pedicura SPA</title>
-<title>Rosma Belleza | Tratamientos de belleza y bienestar</title>
+    <title>Rosma Belleza | Pedicura SPA</title>
     <meta name="title" content="Rosma Belleza | Tratamientos de belleza y bienestar">
     <meta name="description" content="En Rosma Belleza, ofrecemos una amplia gama de tratamientos de belleza y bienestar para ayudarte a sentirte y lucir mejor. ¡Contáctanos!">
     <meta name="author" content="Rosma Belleza">
