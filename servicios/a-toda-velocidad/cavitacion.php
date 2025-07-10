4fbced38ae84816f385200523b69b58fed33d838
Javi Teja <j.teja@cg3innova.es>
2025-07-10 13:37:34 +0200

    cookies ok makey

--- servicios/a-toda-velocidad/cavitacion.php
+++ servicios/a-toda-velocidad/cavitacion.php
@@ -69,7 +69,7 @@
 
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="horariosLabel">Horarios</h4>
+                <h4 class="modal-title" id="horariosLabel">Horario</h4>
             </div>
 
             <div class="modal-body">
